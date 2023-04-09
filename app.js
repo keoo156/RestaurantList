d94a0e48272ed42f36ddec8c7cf569ff2853b3ae
keoo156 <someonewasthere75@gmail.com>
2023-04-09 12:22:48 +0800

    new project

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+const exphbs = require("express-handlebars")
+
+app.engine("handlebars", exphbs({defaultLayout:"main"}))
+app.set("view engine", "handlebars")
+
+app.listen(3000,()=>{
+    console.log("listening")
+})
