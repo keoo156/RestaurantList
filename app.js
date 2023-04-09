3a82b916680c1ae4950721c0909fa46de08ca106
keoo156 <someonewasthere75@gmail.com>
2023-04-09 13:57:08 +0800

    complete

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express")
 const app = express();
 const exphbs = require("express-handlebars")
+const list = require("./restaurant.json")
 
 app.engine("handlebars", exphbs({defaultLayout:"main"}))
 app.set("view engine", "handlebars")
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    
+    res.render("index",{lists:list.results})
+})
+
+app.get("/restaurants/:id",(req,res)=>{
+    const choice = list.results.find(res => res.id.toString() === req.params.id) 
+    res.render("show",{choice})
+})
+
+app.get("/search/", (req,res)=>{
+    const keyword = req.query.keyword
+    const lists = list.results.filter(list=>{
+        return list.category.includes(keyword) || list.name.includes(keyword)
+    })
+    console.log(lists.length)
+    if (keyword.trim() === "" || lists.length === 0){
+        res.render("empty")
+    }else{
+    
+    res.render("index", {lists,keyword})
+}
+})
 
 app.listen(3000,()=>{
     console.log("listening")
